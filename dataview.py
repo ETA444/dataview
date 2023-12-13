b9aeab718060fb6e41af60601f5da61668fa2881
George <georgedreemer@proton.me>
2023-12-13 14:02:20 +0100

    Update dataview.py
    - required packages
    - text colors
    - read_csv custom fun
    - script structure

--- dataview.py
+++ dataview.py
@@ -1,10 +1,40 @@
-# required packages #
+# --- required libraries --- #
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# --- colors for output --- #
+# example usage: print(f"{Colors.GREEN}This is green text!{Colors.RESET}")
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    RESET = '\033[0m'
 
 
-# colors for output #
+# --- accessory functions --- #
+ 
+# [read_csv(): opening a .csv file custom error handling #
+def read_csv(file_path):
+	try:
+		return pd.read_csv(file_path)
+	except Exception as e:
+		print(f"{Colors.RED}An error occured importing the .csv: {e}")
+		return None
+
+# [()]:
 
 
-# accessory functions #
+
+# --- main function --- #
 
 
-# main function #
+
+# --- test area --- #
+
+# test colors print(f"{Colors.GREEN} Hello !{Colors.RESET}")
